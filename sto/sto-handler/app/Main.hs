7abe86a7bbca01624918c5eea7ca72a038580ee1
Niels G. W. Serup <ngws@metanohi.name>
2024-04-18 22:04:03 +0200

    Create a small example using the generated DTD Haskell

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import System.Environment (getArgs)
+import System.IO (stdout, hFlush)
+import Text.XML.HaXml.XmlContent (fReadXml, hPutXml)
+
+import qualified StoMorphology
+
+-- Example: Parse an XML file and re-format it to standard out.
 main :: IO ()
-main = return ()
+main = do
+  args <- getArgs
+  contents <-
+    case args of
+      [morphologyXmlFilePath] -> fReadXml morphologyXmlFilePath :: IO StoMorphology.LexicalResource
+      _ -> error "missing an argument"
+  hPutXml stdout False contents
+  hFlush stdout
