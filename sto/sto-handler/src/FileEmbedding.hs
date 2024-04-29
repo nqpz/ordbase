4adf1d0a3c30841e5ed0eb0daf3c057ad5386a45
Niels G. W. Serup <ngws@metanohi.name>
2024-04-30 00:24:26 +0200

    Concat arrays

--- sto/sto-handler/src/FileEmbedding.hs
+++ sto/sto-handler/src/FileEmbedding.hs
@@ -12,12 +12,21 @@ import Text.XML.HaXml.XmlContent (fReadXml)
 import qualified Data.Array.IArray as ArrI
 import qualified Data.ByteString as BS
 import Data.Store (encode)
+import qualified DynamicArray
+import Control.Monad.ST (runST)
 
 import qualified StoMorphology
 
 extractLexicalEntries :: StoMorphology.LexicalResource -> ArrI.Array Int StoMorphology.LexicalEntry
 extractLexicalEntries (StoMorphology.LexicalResource _ _ _ lexicons) =
   (fmap (\(StoMorphology.Lexicon _ entries) -> entries) lexicons) ArrI.! 1 -- FIXME
+
+arrayConcat :: [ArrI.Array Int StoMorphology.LexicalEntry]
+            -> ArrI.Array Int StoMorphology.LexicalEntry
+arrayConcat arrays = runST $ DynamicArray.runM' $ do
+  DynamicArray.create $ sum $ map (snd . ArrI.bounds) arrays
+  mapM_ (mapM_ DynamicArray.add . ArrI.elems) arrays
+
 
 -- embedFiles :: [FilePath] -> Q Exp
 -- embedFiles paths = do
@@ -32,4 +41,4 @@ storeFiles paths = do
   contents <- flip mapM paths $ \path -> do
     xml <- fReadXml path :: IO StoMorphology.LexicalResource
     return $ extractLexicalEntries xml
-  return $ encode (head contents) -- FIXME
+  return $ encode (arrayConcat contents)
