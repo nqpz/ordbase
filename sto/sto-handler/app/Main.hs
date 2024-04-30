5f904e1776f13809560bea0ab5729547ab697635
Niels G. W. Serup <ngws@metanohi.name>
2024-04-30 23:10:02 +0200

    Generate a compressed byte string

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -5,6 +5,8 @@ import System.IO (stdout, hFlush)
 import Text.XML.HaXml.XmlContent (hPutXml)
 import Data.Store (decodeEx)
 import qualified Data.Array.IArray as ArrI
+import qualified Data.ByteString.Lazy as BS
+import qualified Codec.Compression.Lzma as Lzma
 
 import qualified StoMorphology
 import FileEmbedding (storeFiles)
@@ -15,6 +17,8 @@ main :: IO ()
 main = do
   morphologyXmlFilePaths <- getArgs
   contents <- storeFiles morphologyXmlFilePaths
-  let xml = StoMorphology.Lexicon (ArrI.listArray (1, 0) []) (decodeEx contents)
-  hPutXml stdout False xml
-  hFlush stdout
+  BS.writeFile "morphs.lzma" (Lzma.compress (BS.fromStrict contents))
+  -- contents <- BS.readFile "morphxmls.store"
+  -- let xml = StoMorphology.Lexicon (ArrI.listArray (1, 0) []) (decodeEx contents)
+  -- hPutXml stdout False xml
+  -- hFlush stdout
