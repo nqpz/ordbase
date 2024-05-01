b4e6deeadcfa36364fde26b581d14f165cd6570e
Niels G. W. Serup <ngws@metanohi.name>
2024-05-01 23:40:18 +0200

    Only test the embedded data
    Takes around a minute to traverse and print it all.

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -2,64 +2,64 @@ module Main (main) where
 
 import System.IO (stdout, hFlush)
 import Text.XML.HaXml.XmlContent (hPutXml)
-import Data.Store (decodeEx)
-import qualified Data.ByteString.Lazy as BSL
+-- import Data.Store (decodeEx)
+-- import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Array.IArray as ArrI
-import qualified Codec.Compression.Lzma as Lzma
+-- import qualified Codec.Compression.Lzma as Lzma
 
 import Types
 import qualified StoMorphology
 import qualified StoSyntax
-import StoFiles (morphXmlPaths, syntaxXmlPaths)
-import FileEmbedding (morphsToString, syntaxsToStrings)
+-- import StoFiles (morphXmlPaths, syntaxXmlPaths)
+-- import FileEmbedding (morphsToString, syntaxsToStrings)
 import qualified EmbeddedData
 
 emptyArray :: ImmutableArray e
 emptyArray = ArrI.listArray (1, 0) []
 
-morphsTestFilename :: FilePath
-morphsTestFilename = "morphs.lzma"
+-- morphsTestFilename :: FilePath
+-- morphsTestFilename = "morphs.lzma"
 
--- Example: Parse an XML file and re-format it to standard out.
-morphsStoreAndCompress :: IO ()
-morphsStoreAndCompress = do
-  putStrLn "get paths"
-  paths <- morphXmlPaths
-  mapM_ putStrLn paths
-  putStrLn "make bytes"
-  compressed <- morphsToString paths
-  putStrLn "write"
-  BSL.writeFile morphsTestFilename $ BSL.fromStrict compressed
+-- -- Example: Parse an XML file and re-format it to standard out.
+-- morphsStoreAndCompress :: IO ()
+-- morphsStoreAndCompress = do
+--   putStrLn "get paths"
+--   paths <- morphXmlPaths
+--   mapM_ putStrLn paths
+--   putStrLn "make bytes"
+--   compressed <- morphsToString paths
+--   putStrLn "write"
+--   BSL.writeFile morphsTestFilename $ BSL.fromStrict compressed
 
-morphsDecompressAndUnstoreAndPrint :: IO ()
-morphsDecompressAndUnstoreAndPrint = do
-  putStrLn "read"
-  compressed <- BSL.readFile morphsTestFilename
-  putStrLn "build xml"
-  let lexicalEntries = decodeEx $ BSL.toStrict $ Lzma.decompress compressed
-      xml = StoMorphology.Lexicon emptyArray lexicalEntries
-  putStrLn "write xml"
-  hPutXml stdout False xml
-  hFlush stdout
+-- morphsDecompressAndUnstoreAndPrint :: IO ()
+-- morphsDecompressAndUnstoreAndPrint = do
+--   putStrLn "read"
+--   compressed <- BSL.readFile morphsTestFilename
+--   putStrLn "build xml"
+--   let lexicalEntries = decodeEx $ BSL.toStrict $ Lzma.decompress compressed
+--       xml = StoMorphology.Lexicon emptyArray lexicalEntries
+--   putStrLn "write xml"
+--   hPutXml stdout False xml
+--   hFlush stdout
 
-syntaxTest :: IO ()
-syntaxTest = do
-  paths <- syntaxXmlPaths
-  mapM_ putStrLn paths
-  (lexsCompressed, framesCompressed) <- syntaxsToStrings paths
-  let (lexsDecompressed, framesDecompressed) = (BSL.toStrict $ Lzma.decompress $ BSL.fromStrict lexsCompressed,
-                                                BSL.toStrict $ Lzma.decompress $ BSL.fromStrict framesCompressed)
-      (lexs, frames) = (decodeEx lexsDecompressed,
-                        decodeEx framesDecompressed)
-      xml = StoSyntax.Lexicon emptyArray lexs frames
-  hPutXml stdout False xml
-  hFlush stdout
+-- syntaxTest :: IO ()
+-- syntaxTest = do
+--   paths <- syntaxXmlPaths
+--   mapM_ putStrLn paths
+--   (lexsCompressed, framesCompressed) <- syntaxsToStrings paths
+--   let (lexsDecompressed, framesDecompressed) = (BSL.toStrict $ Lzma.decompress $ BSL.fromStrict lexsCompressed,
+--                                                 BSL.toStrict $ Lzma.decompress $ BSL.fromStrict framesCompressed)
+--       (lexs, frames) = (decodeEx lexsDecompressed,
+--                         decodeEx framesDecompressed)
+--       xml = StoSyntax.Lexicon emptyArray lexs frames
+--   hPutXml stdout False xml
+--   hFlush stdout
 
-testBasics :: IO ()
-testBasics = do
-  morphsStoreAndCompress
-  morphsDecompressAndUnstoreAndPrint
-  syntaxTest
+-- testBasics :: IO ()
+-- testBasics = do
+--   morphsStoreAndCompress
+--   morphsDecompressAndUnstoreAndPrint
+--   syntaxTest
 
 testEmbeddedData :: IO ()
 testEmbeddedData = do
@@ -71,7 +71,7 @@ testEmbeddedData = do
 
 main :: IO ()
 main = do
-  putStrLn "basics"
-  testBasics
+  -- putStrLn "basics"
+  -- testBasics
   putStrLn "embedded data"
   testEmbeddedData
