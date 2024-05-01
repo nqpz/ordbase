63dbd6edb26a8b6df2581230f16b0f2798616780
Niels G. W. Serup <ngws@metanohi.name>
2024-05-01 23:12:09 +0200

    Don't manually compress twice

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -3,15 +3,16 @@ module Main (main) where
 import System.IO (stdout, hFlush)
 import Text.XML.HaXml.XmlContent (hPutXml)
 import Data.Store (decodeEx)
-import qualified Data.ByteString.Lazy as BS
-import qualified Codec.Compression.Lzma as Lzma
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.Array.IArray as ArrI
+import qualified Codec.Compression.Lzma as Lzma
 
 import Types
 import qualified StoMorphology
 import qualified StoSyntax
 import StoFiles (morphXmlPaths, syntaxXmlPaths)
 import FileEmbedding (morphsToString, syntaxsToStrings)
+import qualified EmbeddedData
 
 emptyArray :: ImmutableArray e
 emptyArray = ArrI.listArray (1, 0) []
@@ -26,20 +27,16 @@ morphsStoreAndCompress = do
   paths <- morphXmlPaths
   mapM_ putStrLn paths
   putStrLn "make bytes"
-  raw <- morphsToString paths
-  putStrLn "compress"
-  let compressed = Lzma.compress $ BS.fromStrict raw
+  compressed <- morphsToString paths
   putStrLn "write"
-  BS.writeFile morphsTestFilename compressed
+  BSL.writeFile morphsTestFilename $ BSL.fromStrict compressed
 
 morphsDecompressAndUnstoreAndPrint :: IO ()
 morphsDecompressAndUnstoreAndPrint = do
   putStrLn "read"
-  compressed <- BS.readFile morphsTestFilename
-  putStrLn "decompress"
-  let raw = Lzma.decompress compressed
+  compressed <- BSL.readFile morphsTestFilename
   putStrLn "build xml"
-  let lexicalEntries = decodeEx $ BS.toStrict raw
+  let lexicalEntries = decodeEx $ BSL.toStrict $ Lzma.decompress compressed
       xml = StoMorphology.Lexicon emptyArray lexicalEntries
   putStrLn "write xml"
   hPutXml stdout False xml
@@ -49,19 +46,22 @@ syntaxTest :: IO ()
 syntaxTest = do
   paths <- syntaxXmlPaths
   mapM_ putStrLn paths
-  (lexsRaw, framesRaw) <- syntaxsToStrings paths
-  let (lexsCompressed, framesCompressed) = (Lzma.compress $ BS.fromStrict lexsRaw,
-                                            Lzma.compress $ BS.fromStrict framesRaw)
-      (lexsDecompressed, framesDecompressed) = (Lzma.decompress lexsCompressed,
-                                                Lzma.decompress framesCompressed)
-      (lexs, frames) = (decodeEx $ BS.toStrict lexsDecompressed,
-                        decodeEx $ BS.toStrict framesDecompressed)
+  (lexsCompressed, framesCompressed) <- syntaxsToStrings paths
+  let (lexsDecompressed, framesDecompressed) = (BSL.toStrict $ Lzma.decompress $ BSL.fromStrict lexsCompressed,
+                                                BSL.toStrict $ Lzma.decompress $ BSL.fromStrict framesCompressed)
+      (lexs, frames) = (decodeEx lexsDecompressed,
+                        decodeEx framesDecompressed)
       xml = StoSyntax.Lexicon emptyArray lexs frames
   hPutXml stdout False xml
   hFlush stdout
 
-main :: IO ()
-main = do
+testBasics :: IO ()
+testBasics = do
   morphsStoreAndCompress
   morphsDecompressAndUnstoreAndPrint
   syntaxTest
+
+main :: IO ()
+main = do
+  putStrLn "basics"
+  testBasics
