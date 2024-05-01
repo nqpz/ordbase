a8f3011f14a4502cdf3e6955e7c84b66258461e0
Niels G. W. Serup <ngws@metanohi.name>
2024-05-01 21:34:18 +0200

    Reinstate main example

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -1,24 +1,35 @@
 module Main (main) where
 
-import System.Environment (getArgs)
 import System.IO (stdout, hFlush)
 import Text.XML.HaXml.XmlContent (hPutXml)
 import Data.Store (decodeEx)
-import qualified Data.Array.IArray as ArrI
 import qualified Data.ByteString.Lazy as BS
 import qualified Codec.Compression.Lzma as Lzma
+import qualified Data.Array.IArray as ArrI
 
 import qualified StoMorphology
+import StoFiles (morphXmlPaths)
 import FileEmbedding (morphsToLexiconString)
--- import MorphEmbed (morphs)
+
+testFilename :: FilePath
+testFilename = "morphs.lzma"
 
 -- Example: Parse an XML file and re-format it to standard out.
+compress :: IO ()
+compress = do
+  raw <- morphsToLexiconString =<< morphXmlPaths
+  let compressed = Lzma.compress $ BS.fromStrict raw
+  BS.writeFile testFilename compressed
+
+decompress :: IO ()
+decompress = do
+  compressed <- BS.readFile testFilename
+  let raw = decodeEx $ BS.toStrict compressed
+  let xml = StoMorphology.Lexicon (ArrI.listArray (1, 0) []) raw
+  hPutXml stdout False xml
+  hFlush stdout
+
 main :: IO ()
 main = do
-  morphologyXmlFilePaths <- getArgs
-  contents <- morphsToLexiconString morphologyXmlFilePaths
-  BS.writeFile "morphs.lzma" (Lzma.compress (BS.fromStrict contents))
-  -- contents <- BS.readFile "morphxmls.store"
-  -- let xml = StoMorphology.Lexicon (ArrI.listArray (1, 0) []) (decodeEx contents)
-  -- hPutXml stdout False xml
-  -- hFlush stdout
+  compress
+  decompress
