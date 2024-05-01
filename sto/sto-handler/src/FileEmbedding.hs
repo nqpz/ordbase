41324f7b75107faf768abb7b194ad26bce52bdec
Niels G. W. Serup <ngws@metanohi.name>
2024-05-01 22:16:20 +0200

    Support embedding syntax xmls

--- sto/sto-handler/src/FileEmbedding.hs
+++ sto/sto-handler/src/FileEmbedding.hs
@@ -3,6 +3,8 @@
 module FileEmbedding
   ( morphsToString
   , embedMorphs
+  , syntaxsToStrings
+  , embedSyntaxs
   ) where
 
 import Language.Haskell.TH
@@ -12,13 +14,27 @@ import Data.Store (encode)
 import ArrayUtils
 
 import qualified StoMorphology
+import qualified StoSyntax
 
 morphsToString :: [FilePath] -> IO ByteString
 morphsToString morphPaths = do
   contents <- flip mapM morphPaths $ \path -> do
     xml <- fReadXml path :: IO StoMorphology.LexicalResource
-    return $ StoMorphology.extractLexicalEntries xml
-  return $ encode (arrayConcat contents)
+    pure $ StoMorphology.extractLexicalEntries xml
+  pure $ encode $ arrayConcat contents
 
 embedMorphs :: [FilePath] -> Q Exp
 embedMorphs morphPaths = [| runIO (morphsToLexiconString morphPaths) |]
+
+
+syntaxsToStrings :: [FilePath] -> IO (ByteString, ByteString)
+syntaxsToStrings syntaxPaths = do
+  contents <- flip mapM syntaxPaths $ \path -> do
+    xml <- fReadXml path :: IO StoSyntax.LexicalResource
+    pure (StoSyntax.extractLexicalEntries xml, StoSyntax.extractSubcategorizationFrames xml)
+  let (lexicalEntries, subCategorizationFrames) = unzip contents
+  pure (encode (arrayConcat lexicalEntries),
+        encode (arrayConcat subCategorizationFrames))
+
+embedSyntaxs :: [FilePath] -> Q Exp
+embedSyntaxs syntaxPaths = [| runIO (syntaxsToStrings syntaxPaths) |]
