41b655f662b22d06a44d4c14ff0ca215e4c2465f
Niels G. W. Serup <ngws@metanohi.name>
2024-05-01 22:56:54 +0200

    Also embed with compression

--- sto/sto-handler/src/EmbeddedData.hs
+++ sto/sto-handler/src/EmbeddedData.hs
@@ -5,8 +5,10 @@ module EmbeddedData
   , syntaxSubcategorizationFrames
   ) where
 
+import qualified Data.ByteString.Lazy as BSL
 import Data.ByteString (ByteString)
 import Data.Store (decodeEx)
+import qualified Codec.Compression.Lzma as Lzma
 
 import StoFiles (morphXmlPaths, syntaxXmlPaths)
 import FileEmbedding (embedMorphs, embedSyntaxs)
@@ -14,17 +16,20 @@ import Types
 import qualified StoMorphology
 import qualified StoSyntax
 
+decompress :: ByteString -> ByteString
+decompress = BSL.toStrict . Lzma.decompress . BSL.fromStrict
+
 morphologyLexicalEntriesString :: ByteString
 morphologyLexicalEntriesString = $(embedMorphs morphXmlPaths)
 
 morphologyLexicalEntries :: ImmutableArray StoMorphology.LexicalEntry
-morphologyLexicalEntries = decodeEx morphologyLexicalEntriesString
+morphologyLexicalEntries = decodeEx $ decompress $ morphologyLexicalEntriesString
 
 syntaxLexicalEntriesAndFramesString :: (ByteString, ByteString)
 syntaxLexicalEntriesAndFramesString = $(embedSyntaxs syntaxXmlPaths)
 
 syntaxLexicalEntries :: ImmutableArray StoSyntax.LexicalEntry
-syntaxLexicalEntries = decodeEx $ fst syntaxLexicalEntriesAndFramesString
+syntaxLexicalEntries = decodeEx $ decompress $ fst syntaxLexicalEntriesAndFramesString
 
 syntaxSubcategorizationFrames :: ImmutableArray StoSyntax.SubcategorizationFrame
-syntaxSubcategorizationFrames = decodeEx $ snd syntaxLexicalEntriesAndFramesString
+syntaxSubcategorizationFrames = decodeEx $ decompress $ snd syntaxLexicalEntriesAndFramesString
--- sto/sto-handler/src/FileEmbedding.hs
+++ sto/sto-handler/src/FileEmbedding.hs
@@ -10,18 +10,23 @@ module FileEmbedding
 import Language.Haskell.TH
 import Text.XML.HaXml.XmlContent (fReadXml)
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as BSL
 import Data.Store (encode)
-import ArrayUtils
+import qualified Codec.Compression.Lzma as Lzma
 
+import ArrayUtils
 import qualified StoMorphology
 import qualified StoSyntax
+
+compress :: ByteString -> ByteString
+compress = BSL.toStrict . Lzma.compress . BSL.fromStrict
 
 morphsToString :: [FilePath] -> IO ByteString
 morphsToString morphPaths = do
   contents <- flip mapM morphPaths $ \path -> do
     xml <- fReadXml path :: IO StoMorphology.LexicalResource
     pure $ StoMorphology.extractLexicalEntries xml
-  pure $ encode $ arrayConcat contents
+  pure $ compress $ encode $ arrayConcat contents
 
 embedMorphs :: IO [FilePath] -> Q Exp
 embedMorphs paths = runIO $ do
@@ -34,8 +39,8 @@ syntaxsToStrings syntaxPaths = do
     xml <- fReadXml path :: IO StoSyntax.LexicalResource
     pure (StoSyntax.extractLexicalEntries xml, StoSyntax.extractSubcategorizationFrames xml)
   let (lexicalEntries, subCategorizationFrames) = unzip contents
-  pure (encode (arrayConcat lexicalEntries),
-        encode (arrayConcat subCategorizationFrames))
+  pure (compress $ encode $ arrayConcat lexicalEntries,
+        compress $ encode $ arrayConcat subCategorizationFrames)
 
 embedSyntaxs :: IO [FilePath] -> Q Exp
 embedSyntaxs paths = runIO $ do
