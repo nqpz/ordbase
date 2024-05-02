c22277814c64c722a68e9b825951ff3d357ed534
Niels G. W. Serup <ngws@metanohi.name>
2024-05-02 21:43:57 +0200

    Start using args

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -1,10 +1,24 @@
 module Main (main) where
 
+import System.Environment (getArgs)
 import qualified EmbeddedData as ED
 import qualified DataAnalysis as DA
 
-main :: IO ()
-main = do
+putLengths :: IO ()
+putLengths = do
   putStrLn (show (DA.analyzeLength ED.morphologyLexicalEntries) ++ " morphology lexical entries")
   putStrLn (show (DA.analyzeLength ED.syntaxLexicalEntries) ++ " syntax lexical entries")
   putStrLn (show (DA.analyzeLength ED.syntaxSubcategorizationFrames) ++ " syntax subcategorization frames")
+
+putData :: IO ()
+putData = do
+  DA.putMorphData ED.morphologyLexicalEntries
+  DA.putSyntaxData ED.syntaxLexicalEntries ED.syntaxSubcategorizationFrames
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["lengths"] -> putLengths
+    ["data"] -> putData
+    _ -> error "unknown argument"
