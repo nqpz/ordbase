fe61cbcb0fd2fcdbec455fd879091feaaaa8b28b
Niels G. W. Serup <ngws@metanohi.name>
2024-05-02 20:35:09 +0200

    Move printing into DataAnalysis

--- sto/sto-handler/app/Main.hs
+++ sto/sto-handler/app/Main.hs
@@ -1,84 +1,10 @@
 module Main (main) where
 
-import System.IO (stdout, hFlush)
-import Text.XML.HaXml.XmlContent (hPutXml)
--- import Data.Store (decodeEx)
--- import qualified Data.ByteString.Lazy as BSL
-import qualified Data.Array.IArray as ArrI
--- import qualified Codec.Compression.Lzma as Lzma
-
-import Types
-import qualified StoMorphology
-import qualified StoSyntax
--- import StoFiles (morphXmlPaths, syntaxXmlPaths)
--- import FileEmbedding (morphsToString, syntaxsToStrings)
-import qualified EmbeddedData
-import qualified DataAnalysis
-
-emptyArray :: ImmutableArray e
-emptyArray = ArrI.listArray (1, 0) []
-
--- morphsTestFilename :: FilePath
--- morphsTestFilename = "morphs.lzma"
-
--- -- Example: Parse an XML file and re-format it to standard out.
--- morphsStoreAndCompress :: IO ()
--- morphsStoreAndCompress = do
---   putStrLn "get paths"
---   paths <- morphXmlPaths
---   mapM_ putStrLn paths
---   putStrLn "make bytes"
---   compressed <- morphsToString paths
---   putStrLn "write"
---   BSL.writeFile morphsTestFilename $ BSL.fromStrict compressed
-
--- morphsDecompressAndUnstoreAndPrint :: IO ()
--- morphsDecompressAndUnstoreAndPrint = do
---   putStrLn "read"
---   compressed <- BSL.readFile morphsTestFilename
---   putStrLn "build xml"
---   let lexicalEntries = decodeEx $ BSL.toStrict $ Lzma.decompress compressed
---       xml = StoMorphology.Lexicon emptyArray lexicalEntries
---   putStrLn "write xml"
---   hPutXml stdout False xml
---   hFlush stdout
-
--- syntaxTest :: IO ()
--- syntaxTest = do
---   paths <- syntaxXmlPaths
---   mapM_ putStrLn paths
---   (lexsCompressed, framesCompressed) <- syntaxsToStrings paths
---   let (lexsDecompressed, framesDecompressed) = (BSL.toStrict $ Lzma.decompress $ BSL.fromStrict lexsCompressed,
---                                                 BSL.toStrict $ Lzma.decompress $ BSL.fromStrict framesCompressed)
---       (lexs, frames) = (decodeEx lexsDecompressed,
---                         decodeEx framesDecompressed)
---       xml = StoSyntax.Lexicon emptyArray lexs frames
---   hPutXml stdout False xml
---   hFlush stdout
-
--- testBasics :: IO ()
--- testBasics = do
---   morphsStoreAndCompress
---   morphsDecompressAndUnstoreAndPrint
---   syntaxTest
-
-testEmbeddedData :: IO ()
-testEmbeddedData = do
-  hPutXml stdout False $ StoMorphology.Lexicon emptyArray EmbeddedData.morphologyLexicalEntries
-  hFlush stdout
-
-  hPutXml stdout False $ StoSyntax.Lexicon emptyArray (EmbeddedData.syntaxLexicalEntries) (EmbeddedData.syntaxSubcategorizationFrames)
-  hFlush stdout
-
-test :: IO ()
-test = do
-  -- putStrLn "basics"
-  -- testBasics
-  putStrLn "embedded data"
-  testEmbeddedData
+import qualified EmbeddedData as ED
+import qualified DataAnalysis as DA
 
 main :: IO ()
 main = do
-  putStrLn (show (DataAnalysis.analyze EmbeddedData.morphologyLexicalEntries) ++ " morphology lexical entries")
-  putStrLn (show (DataAnalysis.analyze EmbeddedData.syntaxLexicalEntries) ++ " syntax lexical entries")
-  putStrLn (show (DataAnalysis.analyze EmbeddedData.syntaxSubcategorizationFrames) ++ " syntax subcategorization frames")
+  putStrLn (show (DA.analyze ED.morphologyLexicalEntries) ++ " morphology lexical entries")
+  putStrLn (show (DA.analyze ED.syntaxLexicalEntries) ++ " syntax lexical entries")
+  putStrLn (show (DA.analyze ED.syntaxSubcategorizationFrames) ++ " syntax subcategorization frames")
--- sto/sto-handler/src/DataAnalysis.hs
+++ sto/sto-handler/src/DataAnalysis.hs
@@ -1,10 +1,32 @@
 module DataAnalysis
-  ( analyze
+  ( putMorphData
+  , putSyntaxData
+  , analyze
   ) where
 
+import System.IO (stdout, hFlush)
+import Text.XML.HaXml.XmlContent (hPutXml)
 import qualified Data.Array.IArray as ArrI
 
 import Types
+import qualified StoMorphology
+import qualified StoSyntax
+
+emptyArray :: ImmutableArray e
+emptyArray = ArrI.listArray (1, 0) []
+
+putMorphData :: ImmutableArray StoMorphology.LexicalEntry
+             -> IO ()
+putMorphData entries = do
+  hPutXml stdout False $ StoMorphology.Lexicon emptyArray entries
+  hFlush stdout
+
+putSyntaxData :: ImmutableArray StoSyntax.LexicalEntry
+              -> ImmutableArray StoSyntax.SubcategorizationFrame
+              -> IO ()
+putSyntaxData entries frames = do
+  hPutXml stdout False $ StoSyntax.Lexicon emptyArray entries frames
+  hFlush stdout
 
 analyze :: ImmutableArray e -> Int
 analyze entries = snd $ ArrI.bounds entries
