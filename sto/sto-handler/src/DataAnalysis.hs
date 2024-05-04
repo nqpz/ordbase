ddb053861f4217a2849ebd6feb2a7d5b9f532577
Niels G. W. Serup <ngws@metanohi.name>
2024-05-04 12:05:23 +0200

    Start extracting useful morphology information

--- sto/sto-handler/src/DataAnalysis.hs
+++ sto/sto-handler/src/DataAnalysis.hs
@@ -10,9 +10,12 @@ import Text.XML.HaXml.XmlContent (hPutXml)
 import qualified Data.Array.IArray as ArrI
 import Data.Foldable (foldl')
 import Control.Applicative ((<|>))
-import Control.Monad (guard)
+import Control.Monad (guard, forM_)
+import Data.Maybe (fromJust)
 import Data.Text (Text)
+import Data.Char (toLower)
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 
 import Types
 import qualified StoMorphology
@@ -42,7 +45,7 @@ escape source target = map (\c -> if c == source then target else c)
 
 fixId :: Maybe String -> String
 fixId Nothing = error "ID must always be present"
-fixId (Just ('G' : 'M' : 'U' : '_' : mainPart)) = escape ',' '/' mainPart
+fixId (Just ('G' : 'M' : 'U' : '_' : mainPart)) = map toLower $ escape ',' '/' mainPart
 fixId (Just _) = error "Assumed all ids start with GMU_"
 
 generateWords :: ImmutableArray StoMorphology.LexicalEntry -> IO ()
@@ -51,6 +54,21 @@ generateWords = mapM_ handleEntry
         handleEntry (StoMorphology.LexicalEntry _attrs feats _lemma wordForms _relatedForms) = do
           let wordId = fixId (T.unpack <$> getFeat StoMorphology.Feat_att_id feats)
           putStrLn wordId
+          T.putStrLn $ fromJust $ getFeat StoMorphology.Feat_att_partOfSpeech feats
+          putStrLn "-----"
+          mapM_ handleWordForm wordForms
+          putStrLn "========================================"
+          putStrLn ""
+
+        handleWordForm :: StoMorphology.WordForm -> IO ()
+        handleWordForm (StoMorphology.WordForm mainFeats formRepresentations) = do
+          forM_ mainFeats $ \feat -> do
+            putStr (show $ StoMorphology.featAtt feat)
+            putStr " = "
+            T.putStrLn $ StoMorphology.featVal feat
+          forM_ formRepresentations $ \(StoMorphology.FormRepresentation reprFeats) ->
+            T.putStrLn $ fromJust $ getFeat StoMorphology.Feat_att_writtenForm reprFeats
+          putStrLn "----------------------------------------"
 
         getFeat :: StoMorphology.Feat_att -> ImmutableArray StoMorphology.Feat -> Maybe Text
         getFeat att = foldl' (\prev feat -> prev <|> do
