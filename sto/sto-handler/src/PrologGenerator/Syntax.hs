669e01149caed8afa4971dc1edb3e9e326295073
Niels G. W. Serup <ngws@metanohi.name>
2024-05-16 21:13:10 +0200

    Start outputting frame data

--- sto/sto-handler/src/PrologGenerator/Syntax.hs
+++ sto/sto-handler/src/PrologGenerator/Syntax.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module PrologGenerator.Syntax
   ( generateProlog
   ) where
 
+import qualified Data.List as L
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Maybe (fromJust)
 import Data.Foldable (foldl')
 import Control.Applicative ((<|>))
-import Control.Monad (when, guard, forM_)
+import Control.Monad (guard, forM_)
+import qualified Data.Array.IArray as ArrI
 
 import Types
 import PrologGenerator
@@ -19,11 +22,20 @@ generateProlog :: ImmutableArray StoSyntax.LexicalEntry
                -> ImmutableArray StoSyntax.SubcategorizationFrame
                -> IO ()
 generateProlog entries frames = do
-  generateEntries entries
+  -- generateEntries entries
   generateFrames frames
 
 fixFrame :: Text -> Text
 fixFrame frame = T.concat [ "kind_", T.replace "-" "_" frame]
+
+getFeat feats = fromJust . getFeat' feats
+
+getFeat' :: ImmutableArray StoSyntax.Feat
+         -> StoSyntax.Feat_att -> Maybe Text
+getFeat' feats att = foldl' (\prev feat -> prev <|> do
+                                guard $ StoSyntax.featAtt feat == att
+                                pure $ StoSyntax.featVal feat)
+                     Nothing feats
 
 generateEntries :: ImmutableArray StoSyntax.LexicalEntry -> IO ()
 generateEntries = mapM_ handleEntry
@@ -39,14 +51,42 @@ generateEntries = mapM_ handleEntry
                 ]
           where getLexFeat = getFeat lexFeats
 
-        getFeat feats = fromJust . getFeat' feats
-
-        getFeat' :: ImmutableArray StoSyntax.Feat
-                 -> StoSyntax.Feat_att -> Maybe Text
-        getFeat' feats att = foldl' (\prev feat -> prev <|> do
-                                        guard $ StoSyntax.featAtt feat == att
-                                        pure $ StoSyntax.featVal feat)
-                             Nothing feats
+type ArgumentWithIndex = (Int, ImmutableArray StoSyntax.Feat)
 
 generateFrames :: ImmutableArray StoSyntax.SubcategorizationFrame -> IO ()
-generateFrames frames = pure ()
+generateFrames = mapM_ handleFrame
+  where handleFrame :: StoSyntax.SubcategorizationFrame -> IO ()
+        handleFrame (StoSyntax.SubcategorizationFrame attrs _feats lexemeProperty syntacticArguments) =
+                     let frame = fixFrame $ fromJust $ StoSyntax.subcategorizationFrameId attrs
+                         partOfSpeech = do
+                           StoSyntax.LexemeProperty lexemePropertyFeats <- lexemeProperty
+                           getFeat' lexemePropertyFeats StoSyntax.Feat_att_partOfSpeech
+                         argumentGroups = map (map snd)
+                                          $ unflattenArguments
+                                          $ groupArguments
+                                          $ map makeArgument
+                                          $ ArrI.elems syntacticArguments
+                     in do
+                       T.putStrLn frame
+                       print partOfSpeech
+                       mapM_ (\group -> mapM_ print group >> T.putStrLn "") argumentGroups
+                       T.putStrLn ""
+
+        makeArgument :: StoSyntax.SyntacticArgument -> ArgumentWithIndex
+        makeArgument (StoSyntax.SyntacticArgument _attrs feats) =
+          (read $ T.unpack $ getFeat feats StoSyntax.Feat_att_positionNumber,
+           feats)
+
+        groupArguments :: [ArgumentWithIndex] -> [[ArgumentWithIndex]]
+        groupArguments = \case
+          args@((i, _) : _) ->
+            let (group, rest) = L.span (\(j, _) -> i == j) args
+            in group : groupArguments rest
+          [] -> []
+
+        unflattenArguments :: [[ArgumentWithIndex]] -> [[ArgumentWithIndex]]
+        unflattenArguments = \case
+          group : groups -> do
+            arg <- group
+            map (arg :) $ unflattenArguments groups
+          [] -> [[]]
